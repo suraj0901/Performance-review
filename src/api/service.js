576427eed40ada4f4111235e4efdc40e87c49f6c
keshav-aham <keshav.takatrao@aham.health>
2024-05-13 11:00:53 +0530

    fix bug

--- src/api/service.js
+++ src/api/service.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://performance-review-backend.onrender.com";
 const fetcher = axios.create({
   baseURL,
 });
